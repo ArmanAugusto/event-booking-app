c30325dc2522de453777be6f95b515d1150f847e
Arman Augusto <arman.augusto@gmail.com>
2022-01-08 00:25:57 -0800

    added examples of printing specific datatypes

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var eventName = "Developer Conference"
 	const conferenceTickets = 100
+	var eventName = "Developer Conference"
 	var remainingTickets = 100
 
-	fmt.Println("\nWelcome to our", eventName, "Booking Application")
-	fmt.Println("\nWe have", remainingTickets, "available tickets remaining out of", conferenceTickets)
-	fmt.Println("\nGet your tickets here to attend")
-	fmt.Println("")
+	fmt.Printf("\n\nWelcome to our %s Booking Application\n\n", eventName)
+	fmt.Printf("We have %d available tickets remaining out of %d.\n", remainingTickets, conferenceTickets)
+	fmt.Printf("Get your tickets here to attend.")
+	fmt.Printf("\n\n")
+
+	var userName string
+	var userTickets int
+
+	userName = "dev23"
+	userTickets = 2
+	fmt.Printf("User %s booked %d tickets.\n\n\n", userName, userTickets)
 }
